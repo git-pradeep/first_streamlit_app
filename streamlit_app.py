3795bcfd4e906dd29ff2278e97748d95cffed814
git-pradeep <85672415+git-pradeep@users.noreply.github.com>
2022-11-15 15:31:08 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,13 +5,7 @@ import requests
 from urllib.error import URLError
 
 
-streamlit.title('My Parents New Healthy Diner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
-streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
-streamlit.text('🐔 Hard-Boiled Free-Range Egg')
-streamlit.text('🥑🍞 Avocado Toast')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
 
 streamlit.header("The fruit load list contains:")
@@ -39,7 +33,13 @@ if streamlit.button('Add a Fruit to the List'):
 
 streamlit.stop()
 
-   
+streamlit.title('My Parents New Healthy Diner')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')   
    
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
