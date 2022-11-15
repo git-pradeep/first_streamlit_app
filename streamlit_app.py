a1fcb6bd81d1bb88485392957d61508796d3d69f
git-pradeep <85672415+git-pradeep@users.noreply.github.com>
2022-11-15 17:16:28 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,6 +22,7 @@ try:
     streamlit.dataframe(back_from_function)
 except URLError as e:
   streamlit.error()
+
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list_values ('jackfruit')")    
